01613f3505fef7b5fa5a6e306d537011a7c52e12
Huradil <nuradiltolokov@gmail.com>
2023-05-03 13:26:45 +0200

    add corsheaders to settings

--- coffeeProject/settings.py
+++ coffeeProject/settings.py
@@ -130,3 +130,8 @@ MEDIA_ROOT=BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
