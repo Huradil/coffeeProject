9b052e53d7c3e253f0a35dd73a928db3a8d98dc7
Apal0n <elnuro05@mail.ru>
2023-05-03 17:16:11 +0600

    test commit

--- coffee/models.py
+++ coffee/models.py
@@ -10,5 +10,5 @@ class Menu(models.Model):
     description=models.CharField(max_length=100)
     price=models.FloatField()
     category=models.OneToOneField(Category,on_delete=models.CASCADE,related_name='menu')
-    image=models.ImageField
+    image=models.ImageField.
 
