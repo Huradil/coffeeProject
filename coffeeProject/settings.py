d6322b42a53905596755df23124cf78629bc9279
Huradil <nuradiltolokov@gmail.com>
2023-05-04 20:28:07 +0200

    cors and debug settings

--- coffeeProject/settings.py
+++ coffeeProject/settings.py
@@ -131,7 +131,9 @@ MEDIA_ROOT=BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
